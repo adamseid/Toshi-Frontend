63e4b4bd5ded5b75ec8dde57b14c3f2c4a16dcdb
Gio Moros <gio@flahmingo.com>
2022-12-27 20:31:56 -0700

    Fix: :bug: (websocket) changing the websocket configuration

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -32,7 +32,8 @@ const default_state = {
 
 export default class Overview extends Component {
 
-    ws = new WebSocket('ws://18.232.137.221:8000/ws/toshi/')
+    // ws = new WebSocket('wss://18.232.137.221:8000/ws/toshi/')
+    ws = new WebSocket('wss://build-loadb-19gvvsyoyvbl1-325879419.us-east-1.elb.amazonaws.com/ws/toshi/')
 
     constructor(props) {
         super(props)
