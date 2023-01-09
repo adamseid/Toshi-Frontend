5d9592c0a49986d6b813b3356cca77583ccc6bfc
adamseid <dmsd2916@gmail.com>
2023-01-09 15:59:46 -0800

    fifth push attempt for live changes fixes, improving speed and multiple user glitch

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+var cors = require('cors')
 root.render(
     <App />
 );
