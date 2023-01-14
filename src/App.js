32454bd3a4a6bbc86bc5a81100fe3994f28731a9
adamseid <dmsd2916@gmail.com>
2023-01-13 20:46:32 -0800

    micheal presentation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Test from './components/Test'
 import Overview from './components/Overview';
 import Profile from './components/Profile';
 import Mobile from './components/Mobile';
+import Account from "./components/Account"
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Overview />} />
           <Route exact path='/profile' element={<Profile />}/>
+          <Route exact path='/account' element={<Account />}/>
           <Route exact path='/mobile' element={<Mobile />}/>
         </Routes>
       </Router>
