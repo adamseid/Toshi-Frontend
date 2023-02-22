9be0b09c0fdf2b0ce1ab0c4f0ef4f71ad9ec0f4b
adamseid <dmsd2916@gmail.com>
2023-02-21 16:40:19 -0800

    changed wallet address on profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -81,7 +81,8 @@ export default class Profile extends Component {
           if(result[0].length > 0){
             this.updateWalletAddress()
             // const walletTest=this.state['header']['walletAddress'] = result[0]
-            const walletTest=this.state['header']['walletAddress'] = "0xFDA9d5B343cAd6bCDe6A2D14B4BcF28b17e05B2A"
+            // const walletTest=this.state['header']['walletAddress'] = "0xFDA9d5B343cAd6bCDe6A2D14B4BcF28b17e05B2A"
+            const walletTest=this.state['header']['walletAddress'] = "0xEcd2Ae407bBADaAB3A0A1Bf0c0a009C9f272a8F7"
             this.sendWalletAddress(walletTest);       
           }
         }
