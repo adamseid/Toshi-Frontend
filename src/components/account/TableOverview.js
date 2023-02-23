13f00235641da728c5d7aaf3208004d9204bec2b
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-02-23 13:56:04 +0800

    Change css

--- src/components/account/TableOverview.js
+++ src/components/account/TableOverview.js
@@ -31,30 +31,19 @@ export default class Graph extends Component {
         
             <div className="account-ids">
               <div className="asset-text-data">
-              {/* {
-              this.props.state.accountDetailed.profitDict[0] ? 
-              (Math.round(Object.values(this.props.state.accountDetailed.profitDict[0]).reduce((accumulator, currentValue)=> accumulator + currentValue[0], 0)*100)/100 >= 0 ? 
-              <div className="green">+${Math.abs(Math.round(Object.values(this.props.state.accountDetailed.profitDict[0]).reduce((accumulator, currentValue)=> accumulator + currentValue[0], 0)*100)/100)}</div>
-              : <div className="red">-${Math.abs(Math.round(Object.values(this.props.state.accountDetailed.profitDict[0]).reduce((accumulator, currentValue)=> accumulator + currentValue[0], 0)*100)/100)}</div>
-              ) 
-              : <></>
-              } */}
               {
                 this.props.state.profitHistoryOverview.table[this.props.state.time] ? 
                 this.props.state.profitHistoryOverview.table[this.props.state.time][0] >= 0 ?
                 <div className = "green">
-                  {(Math.round(this.props.state.profitHistoryOverview.table[this.props.state.time][0]*100)/100).toFixed(2)}
+                  ${(Math.round(this.props.state.profitHistoryOverview.table[this.props.state.time][0]*100)/100).toFixed(2)}
                 </div>:
                 <div className = "red">
-                  {(Math.round(this.props.state.profitHistoryOverview.table[this.props.state.time][0]*100)/100).toFixed(2)}
+                  ${(Math.round(this.props.state.profitHistoryOverview.table[this.props.state.time][0]*100)/100).toFixed(2)}
                 </div>
                 : <></>
               }
             </div>
             <div className="asset-text-data">
-              {/* {
-              this.props.state.accountDetailed.profitDict[0] ? Object.values(this.props.state.accountDetailed.profitDict[0]).length : <></>
-              } */}
               {
                 this.props.state.profitHistoryOverview.table[this.props.state.time] ?
                 this.props.state.profitHistoryOverview.table[this.props.state.time][1]
@@ -62,9 +51,6 @@ export default class Graph extends Component {
               }
             </div>
             <div className="asset-text-data">
-              {/* {
-              this.props.state.accountDetailed.profitDict[0] ? this.props.state.accountDetailed.tokensProfitable : <></>
-              } */}
               {
                 this.props.state.profitHistoryOverview.table[this.props.state.time] ?
                 this.props.state.profitHistoryOverview.table[this.props.state.time][2]
@@ -73,14 +59,6 @@ export default class Graph extends Component {
             </div>
     
             <div className="asset-text-data">
-              {/* {
-                this.props.state.accountDetailed.profitDict[0] ? 
-                (Math.round(this.props.state.accountDetailed.tokensProfitable / Object.values(this.props.state.accountDetailed.profitDict[0]).length *100 *100)/100 >= 50 ? 
-                <div className="green">{Math.round(this.props.state.accountDetailed.tokensProfitable / Object.values(this.props.state.accountDetailed.profitDict[0]).length *100 *100)/100 + "%"}</div>
-                : <div className="red">{Math.round(this.props.state.accountDetailed.tokensProfitable / Object.values(this.props.state.accountDetailed.profitDict[0]).length *100 *100)/100 + "%"}</div>
-                ) 
-                : <></>
-                } */}
               {this.props.state.profitHistoryOverview.table[this.props.state.time] ? 
               <div className={this.props.state.profitHistoryOverview.table[this.props.state.time][3] > 50 ? "green" : "red"}>
                 {Math.round(this.props.state.profitHistoryOverview.table[this.props.state.time][3]*100)/100}%
@@ -89,22 +67,7 @@ export default class Graph extends Component {
               } 
             </div>
             <div className="asset-text-data red">
-              
-              {/* {this.props.state.accountDetailed.profitDict[0] ? 
-              (Object.values(this.props.state.accountDetailed.tokenDetails[0]).reduce((accumulator, currentValue) => accumulator + currentValue['gasFees'], 0) < 0.01 ? 
-              (
-              <div>
-              <span>$0.0</span>
-              <sub>
-                {this.props.numberOfZeros(Object.values(this.props.state.accountDetailed.tokenDetails[0]).reduce((accumulator, currentValue) => accumulator + currentValue['gasFees'], 0))}
-              </sub>
-              <span>{this.props.convertDecimalFormat(Object.values(this.props.state.accountDetailed.tokenDetails[0]).reduce((accumulator, currentValue) => accumulator + currentValue['gasFees'], 0))}</span>
-              </div>
-              ) 
-              : "$" + (Math.round(Object.values(this.props.state.accountDetailed.tokenDetails[0]).reduce((accumulator, currentValue) => accumulator + currentValue['gasFees'], 0)*100)/100))
-              : <></>
-              } */}
-              {
+              ${
                 this.props.state.profitHistoryOverview.table[this.props.state.time] ?
                 (Math.round(this.props.state.profitHistoryOverview.table[this.props.state.time][4]*100)/100).toFixed(2)
                 : <></>
